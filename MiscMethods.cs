23a363f3cffb539aad76c16504e9a4e82bdecde2
Benjamin Horner <benjaminpnicholls@gmail.com>
2021-04-07 21:58:26 +0100

    Removing blank lines for better formatting.

--- MiscMethods.cs
+++ MiscMethods.cs
@@ -6,7 +6,6 @@ namespace algorithm_assessment_1 {
         public static void PrintIntArrayToScreen(int[] array) {
             PrintIntArrayToScreen(array, 1);
         }
-
         public static void PrintIntArrayToScreen(int[] array, int everyNthValue) {
             if (everyNthValue > 0) {
                 for (int i = 0; i < array.Length; i++) {
@@ -31,7 +30,6 @@ namespace algorithm_assessment_1 {
             list.AddRange(array2);
             return list.ToArray();
         }
-
         public static int[] MergeArrays(int[] array1, int[] array2, int[] array3) {
             return MergeArrays(MergeArrays(array1, array2), array3);
         }
