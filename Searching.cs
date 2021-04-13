88d42e98de28636515401b6648f49ec934022d26
Benjamin Horner <benjaminpnicholls@gmail.com>
2021-04-13 18:01:01 +0100

    Formatting changes for readability purposes.

--- Searching.cs
+++ Searching.cs
@@ -11,16 +11,20 @@ namespace algorithm_assessment_1 {
             _StepsCounter = 0;
         }
 
+
         // Return results only for numberToFind.
         public static string BinarySearchIterativeALL(int[] array, int numberToFind) {
             int left = 0;
             int right = array.Length - 1;
             List<int> indexesOfNumberToFind = new List<int>();
+
             while (left <= right) {
                 _StepsCounter++;
                 int midpoint = (left + right) / 2;
+
                 if (array[midpoint] == numberToFind) {
                     indexesOfNumberToFind.Add(midpoint);
+
                     // Search left and right of midpoint sequentially for any other instance of numberToFind.
                     int pointer = midpoint - 1;
                     if (pointer >= 0) {
@@ -31,6 +35,7 @@ namespace algorithm_assessment_1 {
                             if (pointer < 0) { break; }
                         }
                     }
+
                     pointer = midpoint + 1;
                     if (pointer < array.Length -1) {
                         while (array[pointer] == numberToFind) {
@@ -40,25 +45,35 @@ namespace algorithm_assessment_1 {
                             if (pointer > array.Length - 1) { break; }
                         }
                     }
+
                     break;
                 }
+
                 if (array[midpoint] < numberToFind) { left = midpoint + 1; }
                 else { right = midpoint - 1; }
             }
-            if (indexesOfNumberToFind.Count == 0) { return $"{numberToFind} is not in the data."; }
+
+            if (indexesOfNumberToFind.Count == 0) {
+                return String.Format("{0} is not in the data.", numberToFind);
+            }
+
             // List > array > sort (array) > string > return
-            return $"{numberToFind} is located in the following indexes: {String.Join(", ", Sorting.QuickSort(indexesOfNumberToFind.ToArray()))}.";
+            return String.Format("{0} is located in the following indexes: {1}.", numberToFind, String.Join(", ", Sorting.QuickSort(indexesOfNumberToFind.ToArray())));
         }
+
 
         // Return results for numberToFind unless there is none, then return results for nearest value(s).
         public static string BinarySearchIterativeNEAREST(int[] array, int numberToFind) {
             int left = 0;
             int right = array.Length - 1;
             List<int> indexesOfNumberToFind = new List<int>();
+
             while (left <= right) {
                 int midpoint = (left + right) / 2;
+
                 if (array[midpoint] == numberToFind) {
                     indexesOfNumberToFind.Add(midpoint);
+
                     // Search left and right of midpoint sequentially for any other instance of numberToFind.
                     int pointer = midpoint - 1;
                     if (pointer >= 0) {
@@ -69,6 +84,7 @@ namespace algorithm_assessment_1 {
                             if (pointer < 0) { break; }
                         }
                     }
+
                     pointer = midpoint + 1;
                     if (pointer > array.Length - 1) {
                         while (array[pointer] == numberToFind) {
@@ -78,46 +94,60 @@ namespace algorithm_assessment_1 {
                             if (pointer > array.Length - 1) { break; }
                         }
                     }
+
                     break;
                 }
-                if (array[midpoint] < numberToFind) { left = midpoint + 1; }
-                else { right = midpoint - 1; }
+
+                if (array[midpoint] < numberToFind) {
+                    left = midpoint + 1;
+                } else {
+                    right = midpoint - 1;
+                }
             }
             if (indexesOfNumberToFind.Count == 0) {
                 // Edge cases / out of bounds checks.
                 if (left >= array.Length) {
-                    return $"{numberToFind} is not present in the data. {array[right]} is the closest value. {BinarySearchIterativeALL(array, array[right])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[right], BinarySearchIterativeALL(array, array[right]));
                 } else if (right < 0) {
-                    return $"{numberToFind} is not present in the data. {array[left]} is the closest value. {BinarySearchIterativeALL(array, array[left])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[left], BinarySearchIterativeALL(array, array[left]));
                 }
+
                 // Regular checks.
                 if (array[left] - numberToFind < numberToFind - array[right]) {
-                    return $"{numberToFind} is not present in the data. {array[left]} is the closest value. {BinarySearchIterativeALL(array, array[left])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[left], BinarySearchIterativeALL(array, array[left]));
                 } else if (numberToFind - array[right] < array[left] - numberToFind) {
-                    return $"{numberToFind} is not present in the data. {array[right]} is the closest value. {BinarySearchIterativeALL(array, array[right])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[right], BinarySearchIterativeALL(array, array[right]));
                 } else {
-                    return $"{numberToFind} is not present in the data. {array[right]} and {array[left]} are the closest values." +
-                        $"\n{BinarySearchIterativeALL(array, array[right])}" + $"\n{BinarySearchIterativeALL(array, array[left])}";
+                    return String.Format("{0} is not present in the data. {1} and {2} are the closest values.", numberToFind, array[right], array[left] +
+                        $"\n{BinarySearchIterativeALL(array, array[right])}" +
+                        $"\n{BinarySearchIterativeALL(array, array[left])}");
                 }
             }
+
             // List > array > sort (array) > string > return.
-            return $"{numberToFind} is located in the following indexes: {String.Join(", ", Sorting.QuickSort(indexesOfNumberToFind.ToArray()))}.";
+            return String.Format("{0} is located in the following indexes: {1}.", numberToFind, String.Join(", ", Sorting.QuickSort(indexesOfNumberToFind.ToArray())));
         }
+
 
         public static string SequentialSearchALL(int[] array, int numberToFind) {
             List<int> indexesOfNumberToFind = new List<int>();
+
             // Stop looping if number is higher than numberToFind.
             for (int i = 0; i < array.Length; i++) {
                 if (array[i] > numberToFind ) { break; }
                 if (array[i] == numberToFind) { indexesOfNumberToFind.Add(i); }
                 _StepsCounter++;
             }
-            if (indexesOfNumberToFind.Count == 0) { return $"{numberToFind} is not in the data."; }
-            return $"{numberToFind} is located in the following indexes: {String.Join(", ", indexesOfNumberToFind.ToArray())}.";
+
+            if (indexesOfNumberToFind.Count == 0) {
+                return String.Format("{0} is not in the data.", numberToFind);
+            }
+            return String.Format("{0} is located in the following indexes: {1}.", numberToFind, String.Join(", ", indexesOfNumberToFind.ToArray()));
         }
         public static string SequentialSearchNEAREST(int[] array, int numberToFind) {
             List<int> indexesOfNumberToFind = new List<int>();
             int pointer = 0;  // Used for NEAREST check later.
+
             // Stop looping if number is higher than numberToFind.
             for (int i = 0; i < array.Length; i++) {
                 if (array[i] > numberToFind) { break; }
@@ -125,24 +155,30 @@ namespace algorithm_assessment_1 {
                 pointer++;
                 _StepsCounter++;
             }
+
             if (indexesOfNumberToFind.Count == 0) {
                 // Edge cases / out of bounds checks.
                 if (pointer + 1 >= array.Length) {
-                    return $"{numberToFind} is not present in the data. {array[pointer - 1]} is the closest value. {SequentialSearchALL(array, array[pointer - 1])}";
+                    //return $"{numberToFind} is not present in the data. {array[pointer - 1]} is the closest value. {SequentialSearchALL(array, array[pointer - 1])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[pointer - 1], SequentialSearchALL(array, array[pointer - 1]));
                 } else if (pointer - 1 < 0) {
-                    return $"{numberToFind} is not present in the data. {array[pointer + 1]} is the closest value. {SequentialSearchALL(array, array[pointer + 1])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[pointer + 1], SequentialSearchALL(array, array[pointer + 1]));
                 }
+
                 // Regular checks.
                 if (array[pointer + 1] - numberToFind < numberToFind - array[pointer - 1]) {
-                    return $"{numberToFind} is not present in the data. {array[pointer + 1]} is the closest value. {SequentialSearchALL(array, array[pointer + 1])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[pointer + 1], SequentialSearchALL(array, array[pointer + 1]));
                 } else if (numberToFind - array[pointer - 1] < array[pointer + 1] - numberToFind) {
-                    return $"{numberToFind} is not present in the data. {array[pointer - 1]} is the closest value. {SequentialSearchALL(array, array[pointer - 1])}";
+                    return String.Format("{0} is not present in the data. {1} is the closest value. {2}", numberToFind, array[pointer - 1], SequentialSearchALL(array, array[pointer - 1]));
                 } else {
-                    return $"{numberToFind} is not present in the data. {array[pointer - 1]} and {array[pointer + 1]} are the closest values." +
-                        $"\n{SequentialSearchALL(array, array[pointer - 1])}" + $"\n{SequentialSearchALL(array, array[pointer + 1])}";
+                    return String.Format("{0} is not present in the data. {1} and {2} are the closest values.", numberToFind, array[pointer - 1], array[pointer + 1]) +
+                        $"\n{SequentialSearchALL(array, array[pointer - 1])}" +
+                        $"\n{SequentialSearchALL(array, array[pointer + 1])}";
                 }
             }
-            return $"{numberToFind} is located in the following indexes: {String.Join(", ", indexesOfNumberToFind.ToArray())}.";
+
+            return String.Format("{0} is located in the following indexes: {1}.", numberToFind, String.Join(", ", indexesOfNumberToFind.ToArray()));
         }
+
     }
 }
